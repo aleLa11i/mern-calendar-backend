2ac56e7c892c33eb034f72a0ba44f2c2967116a2
aleLa11i <82682038+aleLa11i@users.noreply.github.com>
2021-10-08 23:32:56 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const {PORT} = process.env;
+const cors = require("cors");
+const Auth = require("./routes/auth");
+const Event = require("./routes/events");
+const app = express();
+const { DBconnect } = require("./db/config");
+
+app.use( express.static("public") );
+app.use( express.json() );
+app.use(cors());
+app.use("/api/auth", Auth );
+app.use("/api/events", Event );
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/build/index.html'));
+});
+
+DBconnect();
+
+app.listen( PORT, () =>{
+    console.log(`Servidor corriendo en puerto ${ PORT }`);
+} )
